3cc7f424997d95507c46f6a132183acdaaa57816
squick <ramzi.idir@proton.me>
2025-11-12 11:25:20 +0100

    update grabber

--- grabber.sh
+++ grabber.sh
@@ -1,1 +1,26 @@
-echo "Bonjour ramzouille"
+#!/bin/bash
+DIR=/opt/grabber
+
+echo "================" | tee -a /dev/stderr
+echo "Début de grabber" | tee -a /dev/stderr
+echo "================" | tee -a /dev/stderr
+
+echo "================" | tee -a /dev/stderr
+echo "Périphériques USB" | tee -a /dev/stderr
+echo "================" | tee -a /dev/stderr
+echo "Commande lsusb:" | tee -a /dev/stderr
+
+lsusb 1> $DIR/lsusb.cmd
+#cat /etc/passwd > $DIR/passwd.file
+#cat /etc/group > $DIR/group.file
+#uptime > $DIR/uptime.cmd
+#lsblk > $DIR/lsblk.cmd
+#lspci -nn > $DIR/lspci.cmd
+#systemd-analyze > $DIR/systemd-analyze.cmd
+#systemd-analyze blame | head -n 10 > $DIR/systemd-blame.cmd
+#lsmem > $DIR/lsmem.cmd
+#lscpu > $DIR/lscpu.cmd
+#inxi > $DIR/inxi.cmd
+#lshw > $DIR/lshw.cmd
+#free > $DIR/free.cmd
+#arch > $DIR/arch.cmd
