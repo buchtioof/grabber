b0694cd7dcab802af225e45d30f1b0b74e954afa
squick <ramzi.idir@proton.me>
2025-12-05 19:14:34 +0100

    maj grabber, error hardware

--- grabber.sh
+++ grabber.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 DIR=/opt/grabber
+SUM="$DIR/summary.txt"
 SUCCESS_LOG=/var/log/grabber-success.log
 ERROR_LOG=/var/log/grabber-error.log
 
@@ -153,7 +154,7 @@ declare -A PARTITIONS_BY_DISK
 
 FILES=(
     "sources_list.file" "/etc/apt/sources.list*"
-    "passwd.file" "/etc/passwd" 
+    "passwd.file" "/etc/passwd"
     "group.file" "/etc/group"
     "/etc-network-interfaces.file" "/etc/network/interfaces"
     "/etc-resolv-conf.file" "/etc/resolv.conf"
@@ -161,27 +162,65 @@ FILES=(
 
 for disk in ${DEVICES[@]}; do
     disk_path="/dev/$disk"
-    disk_parts=$(lsblk -nr -o PKNAME,PATH $disk_path |grep -vE "^\ " |cut -d " " -f 2) 
+    disk_parts=$(lsblk -nr -o PKNAME,PATH $disk_path |grep -vE "^\ " |cut -d " " -f 2)
     PARTITIONS_BY_DISK[$disk]="${disk_parts[@]}"
 done
 
-echo "Combien de disques sur l'ordinateur ? ${#DEVICES[@]}"
+#echo "Combien de disques sur l'ordinateur ? ${#DEVICES[@]}"
 
-echo "keys: ${!PARTITIONS_BY_DISK[@]}"
-echo "values: ${PARTITIONS_BY_DISK[@]}"
+#echo "keys: ${!PARTITIONS_BY_DISK[@]}"
+#echo "values: ${PARTITIONS_BY_DISK[@]}"
 
-echo "DEVICES=${DEVICES[@]}" > $DIR/status.log
+#echo "DEVICES=${DEVICES[@]}" > $DIR/status.log
 
 for disk in ${!PARTITIONS_BY_DISK[@]}; do
     echo "PARTS_$disk=$(printf '%s ' ${PARTITIONS_BY_DISK[$disk]})"
 done
 
-treat_file() {
-    echo "Les arguments : $@"
-    echo "Argument 1 : $1"
-    echo "Argument 2 : $2"
+#treat_file() {
+#    cat $2 | grep '^#' | grep '^$' > $1
+#}
+
+#for file in ${!FILES[@]}; do
+#    treat_file $file "${FILES[$file]}"
+#done
+
+# HARDWARE
+CPU_MODEL=$(lscpu -eMODELNAME | tail -n1)
+CPU_ID=$(sudo dmidecode -t processor | grep ID | cut -d: -f42 | sed 's/^ *//')
+RAM_SIZE=$(lsmem | grep 'Mémoire partagée' | cut -d: -f2 | sed 's/\ //g')
+RAM_GEN=$(sudo dmidecode -t memory | grep Type: | grep -v Unknown | tail -n1 | cut -d: -f2 | sed 's/\ //')
+SIZES=$(lsblk -dnb | grep -v loop | grep -v boot | tr -s " " | cut -d \  -f4)
+STOCKAGE_TOTAL=0
+
+for SIZE in ${sizes[@]}; do
+    STOCKAGE_TOTAL+=$SIZE
+done
+
+STOCKAGE_TOTAL=$(numfmt --to iec $STOCKAGE_TOTAL)
+
+# SOFTWARE
+OS=$(lsb_release -a | grep Description | cut -f2)
+ARCH=$(uname -a | cut -d' ' -f10)
+KERNEL=$(uname -r)
+
+hardware() {
+    echo "[HARDWARE]" > $SUM
+    echo "CPU_MODEL = $CPU_MODEL" >> $SUM
+    echo "CPU_ID = $CPU_ID" >> $SUM
+    echo "RAM_SIZE = $RAM_SIZE" >> $SUM
+    echo "RAM_GEN = $RAM_GEN" >> $SUM
+    echo "STOCKAGE = $STOCKAGE_TOTAL" >> $SUM
 }
 
-for file in ${!FILES[@]}; do
-    treat_file $file ${FILES[$file]}
-done
+software() {
+    echo "[SOFTWARE]" > $SUM
+    echo "OS = $OS" >> $SUM
+    echo "ARCHITECTURE = $ARCH" >> $SUM
+    echo "KERNEL = $KERNEL" >> $SUM
+    echo "DESKTOP = $XDG_CURRENT_DESKTOP" >> $SUM
+    echo "WINDOW MANAGER = $XDG_SESSION_TYPE" >> $SUM
+}
+
+hardware
+software
