db90ae945dcbdd02ae73b8c1f6ac6d109ca33010
ramzouille <idir.ramzi@icloud.com>
2025-12-11 20:01:46 +0100

    test grabber

--- grabber.sh
+++ grabber.sh
@@ -1,151 +1,43 @@
 #!/bin/bash
 
+# ==============================================================================
+	# Script : grabber.sh
+	# Auteur : IDIR Ramzi
+	# Date   : 2025-12-11
+	# Version: 0.1
+	#
+	# Description :
+	#   Grabber est un script bash qui récup toutes les infos
+	#   hardware et software de l'ordinateur hôte
+	#
+	# Usage :
+	#   ./grabber.sh
+	#
+	# Dépendances :
+	#   - droits <<write>> dans le dossier /opt/grabber et /var/log/grabber
+# ==============================================================================
+
+# Def des variables principales
+DATE=$(date +'%Y-%m-%d-%H%M%S')
 DIR=/opt/grabber
-SUM="$DIR/summary.txt"
-SUCCESS_LOG=/var/log/grabber-success.log
-ERROR_LOG=/var/log/grabber-error.log
+DIR_USED=$DIR/$DATE
+SUM="summary.txt"
+DIR_SUM=$DIR_USED/$SUM
+SUCCESS_LOG=$DIR_GRABBER/grabber-success.log
+ERROR_LOG=$DIR_GRABBER/grabber-error.log
 
-tee $SUCCESS_LOG $ERROR_LOG <<EOF1
-++++++++++++++++
-Début de grabber
-++++++++++++++++
-================
-Récupération des informations sur les paquets
-================
-EOF1
+mkdir $DIR_USED
+touch $DIR_SUM
 
+# Texte de démarrage
+hello () {
+echo "++++++++++++++++++++" >> $DIR_SUM
+echo "Démarrage de grabber" >> $DIR_SUM
+echo "lancé le $DATE" >> $DIR_SUM
+echo "++++++++++++++++++++" >> $DIR_SUM
+echo "" >> $DIR_SUM
+}
 
-#Fichier /etc/apt/sources.list
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF2
-================
-Copie du fichier de configuration /etc/apt/sources.list
-================
-EOF2
-
-cat /etc/apt/sources.list 2> >(tee -a $ERROR_LOG) > $DIR/sources-list.file
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF3
-================
-Récupération de la liste des paquets installés
-================
-EOF3
-
-apt list --installed 2> >(tee -a $ERROR_LOG) > $DIR/apt-installed.cmd \
-	&& echo "[OK]: Fichier apt-installed.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de apt-installed.cmd" > tee -a $ERROR_LOG
-
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF4
-================
-Liste des périphériques USB
-================
-EOF4
-
-lsusb 2> >(tee -a $ERROR_LOG) > $DIR/lsusb.cmd \
-	&& echo "[OK]: Fichier lsusb.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de lsusb.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF5
-================
-Informations sur le processeur
-================
-EOF5
-
-lscpu 2> >(tee -a $ERROR_LOG) > $DIR/lscpu.cmd \
-	&& echo "[OK]: Fichier lscpu.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de lscpu.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF6
-================
-Liste des groupes
-================
-EOF6
-
-cat /etc/group 2> >(tee -a $ERROR_LOG) > $DIR/group.file \
-	&& echo "[OK]: Fichier group.file généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de group.file" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF7
-================
-Liste des utilisateurs
-================
-EOF7
-
-cat /etc/passwd 2> >(tee -a $ERROR_LOG) > $DIR/passwd.file \
-	&& echo "[OK]: Fichier passwd.file généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de passwd.file" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF8
-================
-Informations mémoire
-================
-EOF8
-
-lsmem 2> >(tee -a $ERROR_LOG) > $DIR/lsmem.cmd \
-	&& echo "[OK]: Fichier lsmem.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de lsmem.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF9
-================
-Liste du matériel
-================
-EOF9
-
-lspci 2> >(tee -a $ERROR_LOG) > $DIR/lspci.cmd \
-	&& echo "[OK]: Fichier lspci.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de lspci.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF10
-================
-Information démarrage services
-================
-EOF10
-
-systemd-analyze 2> >(tee -a $ERROR_LOG) > $DIR/systemd-analyze.cmd \
-	&& echo "[OK]: Fichier systemd-analyze.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de systemd-analyze.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF11
-================
-Performances démarrage services
-================
-EOF11
-
-systemd-analyze blame 2> >(tee -a $ERROR_LOG) > $DIR/systemd-blame.cmd \
-	&& echo "[OK]: Fichier systemd-analyze.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de systemd-analyze.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF12
-================
-Liste des réseaux
-================
-EOF12
-
-cat /etc/network/interfaces 2> >(tee -a $ERROR_LOG) > $DIR/etc-network-interfaces.file \
-	&& echo "[OK]: Fichier etc-network-interfaces.file généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de etc-network-interfaces.file" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF13
-================
-Disques et Partitions
-================
-EOF13
-
-lsblk 2> >(tee -a $ERROR_LOG) > $DIR/lsblk.cmd \
-	&& echo "[OK]: Fichier lsblk.cmd généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de lsblk.cmd" > tee -a $ERROR_LOG
-
-tee -a $SUCCESS_LOG $ERROR_LOG <<EOF14
-================
-Configuration DNS
-================
-EOF14
-
-cat /etc/resolv.conf 2> >(tee -a $ERROR_LOG) > $DIR/etc-resolv-conf.file \
-	&& echo "[OK]: Fichier etc-resolv-conf.file généré" > tee -a $SUCCES_LOG \
-	|| echo "[ECHEC]: Erreur à la génération de etc-resolv-conf.file" > tee -a $ERROR_LOG
-
-declare -a DEVICES
 mapfile -t DEVICES < <(lsblk -dn -o NAME |grep -v loop)
 
 declare -A FILES
@@ -205,22 +97,23 @@ ARCH=$(uname -a | cut -d' ' -f10)
 KERNEL=$(uname -r)
 
 hardware() {
-    echo "[HARDWARE]" > $SUM
-    echo "CPU_MODEL = $CPU_MODEL" >> $SUM
-    echo "CPU_ID = $CPU_ID" >> $SUM
-    echo "RAM_SIZE = $RAM_SIZE" >> $SUM
-    echo "RAM_GEN = $RAM_GEN" >> $SUM
-    echo "STOCKAGE = $STOCKAGE_TOTAL" >> $SUM
+    echo "[HARDWARE]" > $DIR_SUM
+    echo "CPU_MODEL = $CPU_MODEL" >> $DIR_SUM
+    echo "CPU_ID = $CPU_ID" >> $DIR_SUM
+    echo "RAM_SIZE = $RAM_SIZE" >> $DIR_SUM
+    echo "RAM_GEN = $RAM_GEN" >> $DIR_SUM
+    echo "STOCKAGE = $STOCKAGE_TOTAL" >> $DIR_SUM
 }
 
 software() {
-    echo "[SOFTWARE]" > $SUM
-    echo "OS = $OS" >> $SUM
-    echo "ARCHITECTURE = $ARCH" >> $SUM
-    echo "KERNEL = $KERNEL" >> $SUM
-    echo "DESKTOP = $XDG_CURRENT_DESKTOP" >> $SUM
-    echo "WINDOW MANAGER = $XDG_SESSION_TYPE" >> $SUM
+    echo "[SOFTWARE]" > $DIR_SUM
+    echo "OS = $OS" >> $DIR_SUM
+    echo "ARCHITECTURE = $ARCH" >> $DIR_SUM
+    echo "KERNEL = $KERNEL" >> $DIR_SUM
+    echo "DESKTOP = $XDG_CURRENT_DESKTOP" >> $DIR_SUM
+    echo "WINDOW MANAGER = $XDG_SESSION_TYPE" >> $DIR_SUM
 }
 
+hello
 hardware
 software
