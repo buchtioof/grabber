7f5a13694bd70b89c5f1d4846c9334f0b4cd9437
ramzcaca <ramzcaca@localhost.localdomain>
2026-01-22 11:35:33 +0100

    wip website

--- app.py
+++ app.py
@@ -8,14 +8,7 @@ from grabber import Grabber
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
-
 ordi1 = Grabber()
-
-@app.get("/ordi1", response_class=HTMLResponse)
-async def read_item(request: Request):
-    return templates.TemplateResponse(
-        request=request, name="ordi.html", context={"ordi": ordi1}
-    )
 
 @app.post("/endpoint")
 async def receive_info(request: Request):
@@ -29,13 +22,31 @@ async def receive_info(request: Request):
     except json.JSONDecodeError:
         raise HTTPException(status_code=400, detail="Invalid JSON")
     
-    # Debug
-    print("Data grabbed :", data)
-    
-    ordi1.hostname = data['HARDWARE']['hostname']
-    ordi1.mb_serial = data['HARDWARE']['mb_serial']
+    hw = data["HARDWARE"]
+    sw = data["SOFTWARE"]
 
-    print(f"Hostname is {ordi1.hostname}")
+    ordi1.host = hw["hostname"]
+    ordi1.cpu = hw["cpu_model"]
+    ordi1.cpu_id = hw["cpu_id"]
+    ordi1.cpu_cores_number = hw["cpu_cores"]
+    ordi1.cpu_threads_number = hw["cpu_threads"]
+    ordi1.cpu_frequency_min = hw["cpu_frequency_min"]
+    ordi1.cpu_frequency_cur = hw["cpu_frequency_cur"]
+    ordi1.cpu_frequency_max = hw["cpu_frequency_max"]
+    
+
+    ordi1.os = sw["os"]
+    ordi1.arch = sw["arch"]
+    ordi1.desktop = sw["desktop_env"]
+    ordi1.wm = sw["window_manager"]
+    ordi1.kernel = sw["kernel"]
+
+    print(f"Hostname is {ordi1.host}")
     print(f"Motherboard serial is {ordi1.mb_serial}")
 
     return {"status": "ok"}
+
+
+@app.get("/ordi1") #page affichant les infos de l'ordi
+async def show_info(request: Request):
+    return templates.TemplateResponse("item.html", {"request": request, "ordi": ordi1})
